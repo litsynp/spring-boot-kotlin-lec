3b481fa6422c282991759a28a6e256d11045fa81
litsynp <nocte55is@gmail.com>
2022-08-06 01:53:10 +0900

    build: init project

---
+++ src/main/kotlin/com/litsynp/demo/MvcApplication.kt
@@ -0,0 +1,11 @@
+package com.litsynp.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinLecApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinLecApplication>(*args)
+}
---
+++ src/test/kotlin/com/litsynp/demo/MvcApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.litsynp.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
