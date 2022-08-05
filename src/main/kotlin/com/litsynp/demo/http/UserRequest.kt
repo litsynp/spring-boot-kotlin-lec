24c3ba8db624f49aef67423948870d2d2eb40f1c
litsynp <nocte55is@gmail.com>
2022-08-06 02:12:14 +0900

    feat: add GET lec

---
+++ src/main/kotlin/com/litsynp/demo/http/UserRequest.kt
@@ -0,0 +1,8 @@
+package com.litsynp.demo.http
+
+data class UserRequest(
+    var name: String? = null,
+    var age: Int? = null,
+    var email: String? = null,
+    var address: String? = null
+)
