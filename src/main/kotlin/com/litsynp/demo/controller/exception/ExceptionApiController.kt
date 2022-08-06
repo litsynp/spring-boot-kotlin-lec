5951039a4413b116c33c18614e3b047703bec096
litsynp <nocte55is@gmail.com>
2022-08-07 01:47:38 +0900

    feat: add and return error response

---
+++ src/main/kotlin/com/litsynp/demo/controller/exception/ExceptionApiController.kt
@@ -0,0 +1,128 @@
+package com.litsynp.demo.controller.exception
+
+import com.litsynp.demo.controller.response.ErrorResponse
+import com.litsynp.demo.model.http.UserRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import javax.servlet.http.HttpServletRequest
+import javax.validation.ConstraintViolationException
+import javax.validation.Valid
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@RestController
+@RequestMapping("/api/exception")
+@Validated
+class ExceptionApiController {
+
+    @GetMapping("/hello")
+    fun hello() {
+        val list = mutableListOf<String>()
+        val temp = list[0]
+
+        if (true) {
+            throw RuntimeException("강제 exception 발생")
+        }
+    }
+
+    @GetMapping
+    fun get(
+        @NotBlank
+        @Size(min = 2, max = 6)
+        @RequestParam name: String,
+
+        @Min(10)
+        @RequestParam age: Int
+    ): String {
+        println(name)
+        println(age)
+        return "${name} ${age}"
+    }
+
+    @PostMapping
+    fun post(@Valid @RequestBody userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun methodArgumentNotValidException(
+        e: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        // 1. 에러 분석
+        val errors = mutableListOf<com.litsynp.demo.controller.response.Error>()
+        e.bindingResult.allErrors.forEach { errorObject ->
+            val error = com.litsynp.demo.controller.response.Error().apply {
+                this.field = (errorObject as FieldError).field
+                this.message = errorObject.defaultMessage
+                this.value = errorObject.rejectedValue
+            }
+            errors.add(error)
+        }
+
+        // 2. Error Response
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
+            this.message = "요청에 에러가 발생했습니다."
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors = errors
+        }
+
+        // 3. ResponseEntity
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+    }
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun constraintViolationException(
+        e: ConstraintViolationException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        // 1. 에러 분석
+        val errors = mutableListOf<com.litsynp.demo.controller.response.Error>()
+        e.constraintViolations.forEach {
+            val error = com.litsynp.demo.controller.response.Error().apply {
+                this.field = it.propertyPath.last().name
+                this.message = it.message
+                this.value = it.invalidValue
+            }
+            errors.add(error)
+        }
+
+        // 2. Error Response
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
+            this.message = "요청에 에러가 발생했습니다."
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors = errors
+        }
+
+        // 3. ResponseEntity
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+    }
+
+    // 해당 컨트롤러 안에서만 일어나는 예외만 처리
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun indexOutOfBoundsException(e: IndexOutOfBoundsException): ResponseEntity<String> {  // 200 OK
+        println("Controller exception handler")
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
+    }
+}
